c60fc017bdb67aea0048e8fb41ff94b0ee3cfe58
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-16 09:35:39 +0300

    Create Arithmetic Mean, Geometric Mean, Weighted Mean, Harmonic Mean, Root Mean Square Formula.java

---
+++ Introduction to Statistics/Introduction to Statistics/Mean/Arithmetic Mean, Geometric Mean, Weighted Mean, Harmonic Mean, Root Mean Square Formula.java
@@ -0,0 +1,48 @@
+Sure, here's a brief overview of the five common measures of central tendency in statistics, along with their formulas:
+
+**1. Arithmetic Mean:**
+
+* Also known as the average, it is the sum of all values in a dataset divided by the number of values.
+* Formula: `Arithmetic Mean = (Σx_i) / n`
+* Example: If you have exam scores of 70, 80, 90, and 85, the arithmetic mean is (70 + 80 + 90 + 85) / 4 = 81.25.
+
+[Image of Arithmetic Mean in Statistics]
+
+**2. Geometric Mean:**
+
+* It is the nth root of the product of all values in a dataset.
+* Formula: `Geometric Mean = (√(x_1 * x_2 * ... * x_n)) ^ (1/n)`
+* Example: For the same exam scores, the geometric mean is (√(70 * 80 * 90 * 85)) ^ (1/4) ≈ 80.6.
+
+[Image of Geometric Mean in Statistics]
+
+**3. Weighted Mean:**
+
+* It assigns different weights to different values in a dataset, reflecting their relative importance.
+* Formula: `Weighted Mean = Σ(w_i * x_i) / Σw_i`
+* Example: If the exam scores have weights of 0.2, 0.3, 0.3, and 0.2, respectively, the weighted mean is (0.2 * 70 + 0.3 * 80 + 0.3 * 90 + 0.2 * 85) / (0.2 + 0.3 + 0.3 + 0.2) ≈ 82.5.
+
+[Image of Weighted Mean in Statistics]
+
+**4. Harmonic Mean:**
+
+* It is the reciprocal of the arithmetic mean of the reciprocals of the values in a dataset. 
+* Formula: `Harmonic Mean = n / (1/x_1 + 1/x_2 + ... + 1/x_n)`
+* Example: For the same exam scores, the harmonic mean is 4 / (1/70 + 1/80 + 1/90 + 1/85) ≈ 78.7.
+
+[Image of Harmonic Mean in Statistics]
+
+**5. Root Mean Square (RMS):**
+
+* It is the square root of the arithmetic mean of the squares of the values in a dataset.
+* Formula: `RMS = √((x_1^2 + x_2^2 + ... + x_n^2) / n)`
+* Example: For the same exam scores, the RMS is √((70^2 + 80^2 + 90^2 + 85^2) / 4) ≈ 82.3.
+
+[Image of Root Mean Square in Statistics]
+
+These are just a few basic examples, and each measure of central tendency has its own strengths and weaknesses depending on the context. Choosing the right one for your analysis depends on the specific data and what you want to learn from it.
+
+I hope this helps! Let me know if you have any other questions.
+
+
+  
